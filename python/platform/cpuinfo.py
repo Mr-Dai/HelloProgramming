1b8bc808c982316c41a0d2cb84cd54352d0b8f64
Robert Peng <robert.peng@foxmail.com>
2017-07-20 23:40:29 +0800

    Add some Python code

---
+++ python/platform/cpuinfo.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+"""Print out the /proc/cpuinfo file"""
+
+from __future__ import print_function
+
+with open('/proc/cpuinfo') as f:
+    for line in f:
+        print(line.rstrip('\n'))
